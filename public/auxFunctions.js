68a3587c395c5d818e0b11a64612c625af23b081
Danceny Karla <dancenykarla@gmail.com>
2024-10-18 21:34:49 +0200

    funciones auxiliares

--- public/auxFunctions.js
+++ public/auxFunctions.js
@@ -12,7 +12,7 @@ function baseHtml() {
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>SportShop</title>
-      <link rel="stylesheet" href="styles.css">
+      <link rel="stylesheet" href="/styles.css">
   </head>
   <body>
       <header>
@@ -69,6 +69,9 @@ function getProductCards(products) {
         </div>
       `;
   }
+
+
+
 
   module.exports = {baseHtml, getNavBar, getProductCards, getProductbyId}
 
