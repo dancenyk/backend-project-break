ec3807999d696063ce65e89bd05298159397c7e8
Danceny Karla <dancenykarla@gmail.com>
2024-10-19 13:31:30 +0200

    views login and register

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const admin = require("firebase-admin")
 const firebase = require("./config/firebase.js")
+const cookieParser = require ("cookie-parser");
+
 
 admin.initializeApp({
     credential: admin.credential.cert(firebase)
@@ -15,6 +17,7 @@ const productRoutes = require("./routes/productRoutes")
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/", productRoutes)
 
